90efb4fb229f963a0cb135bb7275bb9773741933
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2022-03-14 16:39:38 -0700

    feat(dogs): create array of dog objects

---
+++ src/assets/dogs.js
@@ -0,0 +1,34 @@
+const dogs = [
+    {
+        id: 0,
+        name: 'Bosco',
+        image: require('../assets/dog-pics/dog1.webp')
+    },
+    {
+        id: 1,
+        name: 'Skip',
+        image: require('../assets/dog-pics/dog2.webp')
+    },
+    {
+        id: 2,
+        name: 'Marche',
+        image: require('../assets/dog-pics/dog3.jpg')
+    },
+    {
+        id: 3,
+        name: 'Barney',
+        image: require('../assets/dog-pics/dog4.jpg')
+    },
+    {
+        id: 4,
+        name: 'Maggie',
+        image: require('../assets/dog-pics/dog5.webp')
+    },
+    {
+        id: 5,
+        name: 'Simone',
+        image: require('../assets/dog-pics/dog6.webp')
+    }
+];
+
+export default dogs;
