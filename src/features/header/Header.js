38231d252f0d97955abff54de2d362d72e1e2e15
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2022-03-14 16:23:22 -0700

    feat(header): add header challenge

---
+++ src/features/header/Header.js
@@ -0,0 +1,20 @@
+/* ------------------- CHALLENGE BEGIN -------------------- */
+
+/*
+ * Create a cool header for your dog museum! Use other sites on
+ * the web for guidelines about how big the header should be.
+ * You're free to design this one to be as simple or as complex as
+ * you want.
+ * 
+ */
+
+export const Header = (props) => {
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+/* ------------------- CHALLENGE END ---------------------- */
